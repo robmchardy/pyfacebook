d4f261266d3f951413d6f01005e6b6c6b89c1dff
Patryk Zawadzki <patrys@pld-linux.org>
2010-09-06 16:54:45 +0200

    get_login_url is back

--- facebook/djangofb/views.py
+++ facebook/djangofb/views.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import direct_to_template
 def facebook_auth(request, template_name='djangofb/auth.html'):
     next = request.REQUEST.get('next', None)
     required_permissions = request.REQUEST.get('required_permissions', None)
-    url = request.facebook.get_login_url(next=next,
+    url = request.facebook.get_auth_url(next=next,
             required_permissions=required_permissions)
     return direct_to_template(request, template_name, {
         'facebook_url': url,
